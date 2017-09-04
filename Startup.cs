dfe59748b681a1d11aad8472c94ee855a8a06ebe
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-09-04 09:06:19 -0500

    Making it so node_modules gets served from the root

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Newtonsoft.Json.Serialization;
-
+using Microsoft.AspNetCore.Http;
 
 namespace electronAngular2{
         public class Startup
@@ -22,7 +22,16 @@ namespace electronAngular2{
         {
             app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
+
+            // static files documented really well here: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/static-files
             app.UseStaticFiles();
+            // we only want one package.json and one set of "node_modules" so map it so the wwwroot can access it
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
+                    System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "node_modules")),
+                RequestPath = new PathString("/node_modules")
+            });
         }
     }
 }
